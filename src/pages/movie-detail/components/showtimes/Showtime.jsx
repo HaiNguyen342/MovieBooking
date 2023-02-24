a6494a33eeb62a8ad376448be3fbcfb8748cd974
Nguyen Minh Hai <allennng342@gmail.com>
2023-02-24 15:51:41 +0700

    render cinema list and movie showtimes

--- src/pages/movie-detail/components/showtimes/Showtime.jsx
+++ src/pages/movie-detail/components/showtimes/Showtime.jsx
@@ -1,6 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchMovieShowtimesApi } from "../../../../services/cinema";
 
 export default function Showtime() {
+  const [movieShowtimes, setMovieShowtimes] = useState({});
+
+  const params = useParams();
+
+  useEffect(() => {
+    getMovieShowtimes();
+  }, []);
+
+  const getMovieShowtimes = async () => {
+    const result = await fetchMovieShowtimesApi(params.id);
+
+    setMovieShowtimes(result.data.content);
+  };
+
+  const renderTabs = () => {
+    return movieShowtimes?.heThongRapChieu?.map((ele, idx) => {
+      return (
+        <a
+          key={ele.maHeThongRap}
+          className={`nav-link text-capitalize ${idx === 0 && "active"}`}
+          data-toggle="pill"
+          href={`#${ele.maHeThongRap}`}
+          role="tab"
+          aria-selected="true"
+        >
+          {ele.tenHeThongRap}
+        </a>
+      );
+    });
+  };
+
+  const renderTabContents = () => {
+    return movieShowtimes?.heThongRapChieu?.map((ele, idx) => {
+      return (
+        <div
+          key={ele.maHeThongRap}
+          className={`tab-pane fade show ${idx === 0 && "active"}`}
+          id={ele.maHeThongRap}
+          role="tabpanel"
+        >
+          {/* cumRapChieu is an array listed inside heThongRapChieu */}
+          {ele?.cumRapChieu?.map((ele) => {
+            return (
+              <div key={ele.maCumRap} className="row mb-5">
+                <div className="col-1">
+                  <img className="img-fluid rounded" src={ele.hinhAnh} />
+                </div>
+                <div className="col-11 pl-0">
+                  <h5>{ele.tenCumRap}</h5>
+                  <span className="text-muted">{ele.diaChi}</span>
+                </div>
+                <div className="col-12">
+                  <div className="row">
+                    {/* lichChieuuuuPhim is an array listed inside cumRapChieu */}
+                    {ele?.lichChieuPhim?.map((ele) => {
+                      return (
+                        <div key={ele.maLichChieu} className="col-3">
+                          <a href="#">{ele.ngayChieuGioChieu}</a>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      );
+    });
+  };
+
   return (
     <div className="col-12 mt-5">
       <div className="row">
@@ -11,97 +84,12 @@ export default function Showtime() {
             role="tablist"
             aria-orientation="vertical"
           >
-            <a
-              className="nav-link text-capitalize active"
-              data-toggle="pill"
-              href="#galaxy"
-              role="tab"
-              aria-selected="true"
-            >
-              Galaxy Cinema
-            </a>
-            <a
-              className="nav-link text-capitalize"
-              data-toggle="pill"
-              href="#bhd"
-              role="tab"
-              aria-selected="false"
-            >
-              BHD Star
-            </a>
+            {renderTabs()}
           </div>
         </div>
         <div className="col-9">
           <div className="tab-content" id="v-pills-tabContent">
-            <div
-              className="tab-pane fade show active"
-              id="galaxy"
-              role="tabpanel"
-            >
-              <div className="row mb-5">
-                <div className="col-1">
-                  <img
-                    className="img-fluid rounded"
-                    src="https://s3img.vcdn.vn/123phim/2021/01/bhd-star-bitexco-16105952137769.png"
-                  />
-                </div>
-                <div className="col-11 pl-0">
-                  <h5>Galaxy Cinema Cineplex - 3/2</h5>
-                  <span className="text-muted">
-                    L5-Vincom 3/2, 3C Đường 3/2, Q.10
-                  </span>
-                </div>
-                <div className="col-12">
-                  <div className="row">
-                    <div className="col-3">
-                      <a href>2022-12-12T09:30:00</a>
-                    </div>
-                    <div className="col-3">
-                      <a href>2022-12-12T09:30:00</a>
-                    </div>
-                    <div className="col-3">
-                      <a href>2022-12-12T09:30:00</a>
-                    </div>
-                    <div className="col-3">
-                      <a href>2022-12-12T09:30:00</a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="row mb-5">
-                <div className="col-1">
-                  <img
-                    className="img-fluid rounded"
-                    src="https://s3img.vcdn.vn/123phim/2021/01/bhd-star-bitexco-16105952137769.png"
-                  />
-                </div>
-                <div className="col-11 pl-0">
-                  <h5>Galaxy Cinema Cineplex - Vincom Thảo Điền</h5>
-                  <span className="text-muted">
-                    L5-Megamall, 159 XL Hà Nội, Q.2
-                  </span>
-                </div>
-                <div className="col-12">
-                  <div className="row">
-                    <div className="col-3">
-                      <a href>2022-12-12T09:30:00</a>
-                    </div>
-                    <div className="col-3">
-                      <a href>2022-05-12T18:12:23</a>
-                    </div>
-                    <div className="col-3">
-                      <a href>2022-05-12T18:12:23</a>
-                    </div>
-                    <div className="col-3">
-                      <a href>2022-05-12T18:12:23</a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="tab-pane fade" id="bhd" role="tabpanel">
-              ...
-            </div>
+            {renderTabContents()}
           </div>
         </div>
       </div>
