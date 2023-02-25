b32653e82772a84fa5cc2b28ed43f3703144e3ae
Nguyen Minh Hai <allennng342@gmail.com>
2023-02-25 13:21:46 +0700

    handle change and submit login infomation functions

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { loginApi } from "../../services/user";
 
 export default function Login() {
+  const [state, setState] = useState({
+    taiKhoan: "",
+    matKhau: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setState({
+      ...state,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const result = await loginApi(state);
+  };
+
   return (
     <div className="w-25 mx-auto py-5">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="">Username</label>
-          <input type="text" className="form-control" />
+          <input
+            type="text"
+            className="form-control"
+            name="taiKhoan"
+            onChange={handleChange}
+          />
         </div>
         <div className="form-group">
           <label htmlFor="">Password</label>
-          <input type="text" className="form-control" />
+          <input
+            type="text"
+            className="form-control"
+            name="matKhau"
+            onChange={handleChange}
+          />
         </div>
         <button className="btn btn-primary">LOGIN</button>
       </form>
